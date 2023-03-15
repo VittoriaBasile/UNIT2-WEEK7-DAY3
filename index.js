8a79140610e12be8202e23548d90271d7e70c816
VittoriaBasile <vichy.basile@gmail.com>
2023-03-15 15:59:21 +0100

    commit

--- index.js
+++ index.js
@@ -6,21 +6,23 @@ fetch("https://striveschool-api.herokuapp.com/books")
     grid.innerHTML = "";
     libreria.forEach((libro) => {
       const col = document.createElement("div");
-      col.className = "col-4";
+      col.className = "col";
       col.innerHTML = `<div id = "card" class="card" style="width: 18rem;">
         <img src="${libro.img}" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">${libro.title}</h5>
           <p class="card-text">${libro.price}</p>
-          
+          <button class="scarta" >SCARTA</button>
         </div>
       </div>`;
       grid.appendChild(col);
-      const scarta = document.createElement("button");
-      col.appendChild(scarta);
-      scarta.addEventListener("click", (event) => {
+    });
+    const scarta = document.querySelectorAll(".scarta");
+    scarta.forEach((button) => {
+      button.addEventListener("click", (event) => {
         event.preventDefault();
-        col.className = "d-none";
+
+        document.querySelector(".col").className = "d-none";
       });
     });
   })
